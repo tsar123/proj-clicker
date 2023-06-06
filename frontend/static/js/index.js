fa7acc7553577c0268df50ad408f9eac4d5d6736
Tsaregorodtsev <65504823+tsar123@users.noreply.github.com>
2023-06-07 00:13:00 +0500

    add frontend

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -42,3 +42,52 @@ function add_boost(parent, boost) {
 
     parent.appendChild(button)
 }
+
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+function buy_boost(boost_id) {
+    const csrftoken = getCookie('csrftoken')
+
+    fetch(`/boost/${boost_id}/`, {
+        method: 'PUT',
+        headers: {
+            "X-CSRFToken": csrftoken,
+            'Content-Type': 'application/json'
+        }
+    }).then(response => {
+        if (response.ok) return response.json()
+        else return Promise.reject(response)
+    }).then(response => {
+        if (response.error) return
+        const old_boost_stats = response.old_boost_stats
+        const new_boost_stats = response.new_boost_stats
+
+        const coinsElement = document.getElementById('coins')
+        coinsElement.innerText = Number(coinsElement.innerText) - old_boost_stats.price
+        const powerElement = document.getElementById('click_power')
+        powerElement.innerText = Number(powerElement.innerText) + old_boost_stats.power
+
+        update_boost(new_boost_stats)
+    }).catch(err => console.log(err))
+}
+
+
+function update_boost(boost) {
+    const boost_node = document.getElementById(`boost_${boost.id}`)
+    boost_node.querySelector('#boost_level').innerText = boost.lvl
+    boost_node.querySelector('#boost_power').innerText = boost.power
+    boost_node.querySelector('#boost_price').innerText = boost.price
+}
